d1a820fbc9b3845044a35a5d44708920a361a711
Shiro R <ha4er@mail.ru>
2026-03-03 17:00:37 +0300

    Clear Program

--- NspdWebService/Program.cs
+++ NspdWebService/Program.cs
@@ -1,14 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseSetting("preventHostingStartup", "True");
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.ListenAnyIP(5000);
-    serverOptions.ListenAnyIP(5001, listenOptions =>
-    {
-        listenOptions.UseHttps();
-    });
-});
 
 if (builder.Environment.IsDevelopment())
 {
@@ -27,9 +19,6 @@ else
 {
     builder.Services.AddRazorPages();
 }
-
-var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>()
-                     ?? new[] { "http://localhost:5001", "http://localhost:5000" };
 
 builder.Services.AddCors(options =>
 {
@@ -70,26 +59,6 @@ app.UseAuthorization();
 app.UseSession();
 
 app.MapRazorPages();
-
-app.MapGet("/api/health", () => new { status = "OK", timestamp = DateTime.UtcNow });
-
-app.MapGet("/api/network", async (HttpContext context) =>
-{
-    var hostName = System.Net.Dns.GetHostName();
-    var ipAddresses = System.Net.Dns.GetHostAddresses(hostName)
-        .Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-        .Select(ip => ip.ToString())
-        .ToList();
-
-    return new
-    {
-        hostName,
-        localIp = context.Connection.LocalIpAddress?.ToString(),
-        remoteIp = context.Connection.RemoteIpAddress?.ToString(),
-        allIps = ipAddresses,
-        port = context.Connection.LocalPort
-    };
-});
 
 app.MapGet("/", () => Results.Redirect("/Map"));
 
